b70b7a5e74d478afacd523aa37494ac2e876a3f9
LAB-USR-AREQUIPA <LAB-USR-AREQUIPA@grupoutp.net>
2025-04-12 13:04:34 -0500

    base del proyecto

---
+++ GestionAuditoria/src/test/java/com/core/GestionAuditoria/GestionAuditoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.GestionAuditoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionAuditoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
