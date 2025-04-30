bbf39f4ab6f3177b33f8764e1d527a5020fb66d3
JPjocar <jpjocar.6@gmail.com>
2025-04-29 19:07:45 -0500

    Agrega repositorio de la entidad Item

---
+++ backend/SysAuditoria/src/main/java/com/auditoriasys/SysAuditorias/repository/ItemRepository.java
@@ -0,0 +1,15 @@
+package com.auditoriasys.SysAuditorias.repository;
+
+import com.auditoriasys.SysAuditorias.entity.Item;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item,Integer>{
+	
+	List<Item> findByCodigoNormaIso (Integer codigoNorma);
+	
+}
