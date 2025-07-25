79052c01c28311620dee8635f9a27113bd69d207
Katherine Mayta Hachiri <114774385+kittymayta@users.noreply.github.com>
2025-07-25 11:31:14 -0500

    Revert "Feature/procesos"

--- frontend/src/pages/Procesos/Coordinador/procesosCordi.jsx
+++
@@ -1,9 +0,0 @@
-const ProcesosCoordinador = () => {
-  return (
-    <div>
-      <h2>Procesos para el coordinador</h2>
-    </div>
-  );
-};
-
-export default ProcesosCoordinador;
--- frontend/src/pages/dashboard/home.jsx
+++
@@ -1,9 +0,0 @@
-
-const Home = () => {
-    return (
-        <div>
-            <h1>Pagina Principal</h1>
-        </div>
-    )
-}
-export default Home;
