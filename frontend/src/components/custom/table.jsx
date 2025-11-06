da90cc2e369761cfd982f2d1f0af8c1f1e9a8baf
JPjocar <92408008+JPjocar@users.noreply.github.com>
2025-11-06 15:22:37 -0500

    Change table header background color to red

--- frontend/src/components/custom/table.jsx
+++ frontend/src/components/custom/table.jsx
@@ -48,7 +48,7 @@ const TableHead = React.forwardRef(({ className, ...props }, ref) => (
   <th
     ref={ref}
     className={cn(
-      "h-10 px-2 text-left align-middle font-medium text-muted-foreground text-white [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px] border bg-custom-blue",
+      "h-10 px-2 text-left align-middle font-medium text-muted-foreground text-white [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px] border bg-[#62060f]",
       className
     )}
     {...props} />
